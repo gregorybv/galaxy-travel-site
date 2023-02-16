59027da66672b80c27c6e76bc221f67743435784
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-16 18:44:16 +0300

    add video component to project

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <div>
-
+      <Navbar />
     </div>
   )
 }
 
 export default Home
-
 
 
