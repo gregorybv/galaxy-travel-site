e98a1675ff98ddb0769f75e2e54ef344e8496b29
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-16 19:52:57 +0300

    add training routes to project

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react'
+import './HeroImageStyles.css'
 
-const HeroImage = () => {
-  return (
-    <div>
-
-    </div>
-  )
+class HeroImage extends Component {
+  render() {
+    return (
+      <div className='hero-img'>
+        <div className='heading'>
+          <h1>{this.props.heading}</h1>
+          <p>{this.props.text}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default HeroImage
---
+++ src/routes/Training.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Training = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Training
+
+
+
